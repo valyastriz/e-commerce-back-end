3fdfd25d5c916c3f34658c537be7117e61202f4e
valyastriz <valyastriz@gmail.com>
2024-07-27 15:45:08 -0400

    added get route by id for tag-reoutes.js

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -16,9 +16,22 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const singleTag = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+
+    if (!singleTag) {
+      res.status(404).json({ message: 'No tag found with that id' });
+    }
+
+    res.status(200).json(singleTag);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {

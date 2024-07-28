071666678506c6093540180dddc22b6e180774b4
valyastriz <valyastriz@gmail.com>
2024-07-28 19:29:58 -0400

    added .get method to find category by id to category-routes.js

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -16,9 +16,20 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const singleCategory = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+    if (!singleCategory) {
+      res.status(404).json({ message: 'No category found with that id'} );
+    }
+    res.status(200).json(singleCategory);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
